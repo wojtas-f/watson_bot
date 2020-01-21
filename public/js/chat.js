acaf5e8785e0b3bb10fd2dbef6de49b2a48aff62
Vulpie <wojtas_f@outlook.com>
2020-01-21 11:33:43 +0100

    style: increase chat width

--- public/js/chat.js
+++ public/js/chat.js
@@ -58,12 +58,12 @@ const userMessage = msg_content => {
     const time = getCurrentTime()
     return `
         <div class="ui grid">
-            <div class="eight wide column">
+            <div class="six wide column">
                 <div class="ui blue message user-input msg user">
                     <p>${msg_content}</p>
                 </div>
             </div>
-            <div class="eight wide column time-display">
+            <div class="ten wide column time-display">
                     <p class="time">${time}</p>
             </div>
         </div>
@@ -75,10 +75,10 @@ const assistMessage = msg_content => {
     return `
     <hr class="break">
         <div class="ui grid">
-            <div class="eight wide column time-display">
+            <div class="ten wide column time-display">
                 <p class="time">${time}</p>
             </div>
-        <div class="eight wide column">
+        <div class="six wide column">
             <div class="ui success message user-input msg assist">
                 <p>${msg_content}</p>
             </div>
