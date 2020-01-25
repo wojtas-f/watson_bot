d44285f975c09a67fbf4c89164689b560b5cc153
Vulpie <wojtas_f@outlook.com>
2020-01-25 22:03:36 +0100

    fix: index path correction

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -3,14 +3,13 @@ const {
     renderIndex,
     wildcard,
     renderChat,
-    clearSession,
-    saveMessageToSession
+    clearSession
 } = require('../controller/pages')
 
 const router = express.Router()
 
 router.route('/chat').get(renderChat)
-router.route('').get(renderIndex)
+router.route('/').get(renderIndex)
 router.route('/session/resetid').post(clearSession)
 router.route('/*').get(wildcard)
 
